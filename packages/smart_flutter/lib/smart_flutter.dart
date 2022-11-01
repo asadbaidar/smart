33d4c0be011ce2ff10ae59910325720844b9cdb4
Muhammad Asad Baidar <asadbaidar@gmail.com>
2022-11-02 00:32:59 +0500

    smart_flutter: ^0.0.6

--- packages/smart_flutter/lib/smart_flutter.dart
+++ packages/smart_flutter/lib/smart_flutter.dart
@@ -1,5 +1,8 @@
+/// smart_flutter
 library smart_flutter;
 
+/// smart_flutter_utils
 export 'package:smart_flutter_utils/smart_flutter_utils.dart';
-export 'package:smart_flutter_widgets/smart_flutter_widgets.dart';
 
+/// smart_flutter_widgets
+export 'package:smart_flutter_widgets/smart_flutter_widgets.dart';
--- packages/smart_flutter/test/smart_flutter_test.dart
+++ packages/smart_flutter/test/smart_flutter_test.dart
@@ -2,6 +2,7 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:smart_flutter/smart_flutter.dart';
 
+/// smart_flutter_test
 void main() {
   group("StringConcatenation extension", () {
     const text = "hello";
