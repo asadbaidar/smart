82d5debe4d560fe761fc50eca886deafb573f346
Muhammad Asad Baidar <asadbaidar@gmail.com>
2024-06-19 04:35:06 +0500

    feat: Add JSON utility methods

---
+++ packages/smart_flutter_utils/lib/src/utils/json.dart
@@ -0,0 +1,43 @@
+part of 'utils.dart';
+
+typedef MapObject = Map<dynamic, dynamic>;
+typedef JsonObject = Map<String, dynamic>;
+typedef JsonArray = List<dynamic>;
+
+extension StringToJson on String {
+  JsonObject? toJsonObject() {
+    final dynamic value = jsonDecode(this);
+    return value is JsonObject ? value : null;
+  }
+
+  JsonObject? get jsonObject => toJsonObject();
+
+  JsonArray? toJsonArray() {
+    final dynamic value = jsonDecode(this);
+    return value is JsonArray ? value : null;
+  }
+
+  JsonArray? get jsonArray => toJsonArray();
+
+  dynamic get json => jsonDecode(this);
+
+  dynamic get jsonOrString {
+    try {
+      return jsonDecode(this);
+    } catch (e) {
+      return this;
+    }
+  }
+}
+
+extension JsonToString on Map<String, dynamic> {
+  String toJsonString() => jsonEncode(this);
+
+  String get jsonString => toJsonString();
+}
+
+extension JsonArrayToString on List<dynamic> {
+  String toJsonString() => jsonEncode(this);
+
+  String get jsonString => toJsonString();
+}
